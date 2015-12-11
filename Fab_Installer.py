2ee46eadbec6d0871a655067b1e3cc04bb3575f6
Solomon <kingds41@gmail.com>
2015-12-10 20:52:12 -0500

    Attempt at Gui

--- Fab_Installer.py
+++ Fab_Installer.py
@@ -6,48 +6,47 @@
 
 import os, sys, getpass, gtk
 
-class fabinstaller(gtk.Window):
+class fabinstaller():
 
     def __init__(self):
         user = getpass.getuser()
         print('Getting information for the GUI. Your User Name is: %s' % (user))
-        gtk.Window.__init__(self)
-        self.button()
+        self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
+        self.window.set_title("Ultimate Fab Installer")
+        self.window.set_default_size(500, 500)
 
-    def button(self):
-        install_btn = gtk.Button("Install")
-        close_btn = gtk.Button("Close")
+        self.window.connect("delete_event", self.delete_event)
 
-        install_btn.set_size_request(100, 40)
-        install_btn.connect("clicked", self.on_button_clicked)
-        close_btn.connect("clicked", self.on_close_btn_clicked)
-        
-        vbox = gtk.VBox(False, 5)
-        hbox = gtk.HBox(True, 5)
-        
-        valign = gtk.Alignment(1, 0, 0, 0)
-        vbox.pack_start(valign)
-        halign = gtk.Alignment(1, 0, 0, 0)
-        
-        hbox.add(install_btn)
-        hbox.add(close_btn)
-        
-        halign.add(hbox)
-        
-        vbox.pack_start(halign, False, False, 5)
+        self.box1 = gtk.HBox(False, 0)
 
-        self.add(vbox)
+        self.window.add(self.box1)
+       
+        self.close_button = gtk.Button("Close Button")
+        self.close_button.connect("clicked", self.on_close_btn_clicked)
 
-    def on_button_clicked(self, widget):
-        print("Hello World")
+        self.box1.pack_start(self.close_button, True, True, 0)
+       
+        self.button2 = gtk.Button("Button 2")
+
+        self.box1.pack_start(self.button2, True, True, 0)
+
+        self.close_button.show()
+        self.button2.show() 
+        self.box1.show()
+        self.window.show()
+
+
+    def delete_event(self, widget, event, data=None):
+        gtk.main_quit()
+        return False
 
     def on_close_btn_clicked(self, widget):
-        print("Closing The Application")
-        sys.exit(1)
+        gtk.main_quit()
 
-app=fabinstaller()
-app.connect("delete-event", gtk.main_quit)
-app.set_default_size(500, 500)
-app.set_title("Ultimate Fab Installer")
-app.show_all()
-gtk.main()
+
+def main():
+   gtk.main()
+
+if __name__ == "__main__":
+   hello = fabinstaller()
+   main()
